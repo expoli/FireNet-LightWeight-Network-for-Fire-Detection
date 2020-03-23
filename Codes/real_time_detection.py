60002cc095a3d7471c8b7c5ecc397694fed7f2c1
expoli <31023767+expoli@users.noreply.github.com>
2020-03-23 18:02:32 +0800

    Revert "Detection test"

--- Codes/real_time_detection.py
+++ Codes/real_time_detection.py
@@ -1,36 +1,17 @@
-import time
-
-import cv2
-import numpy as np
 import tensorflow as tf
 from tensorflow.keras.models import load_model
 from tensorflow.keras.preprocessing.image import img_to_array
-
-
-def init_gpu():
-    gpus = tf.config.experimental.list_physical_devices('GPU')
-    if gpus:
-        try:
-            # Currently, memory growth needs to be the same across GPUs
-            for gpu in gpus:
-                tf.config.experimental.set_memory_growth(gpu, True)
-            logical_gpus = tf.config.experimental.list_logical_devices('GPU')
-            print(len(gpus), "Physical GPUs,", len(logical_gpus), "Logical GPUs")
-        except RuntimeError as e:
-            # Memory growth must be set before GPUs have been initialized
-            print(e)
-    return 0
-
-
-init_gpu()
+import numpy as np
+import cv2
+import time
 
 # loading the stored model from file
-model = load_model('my_model.h5')
+model=load_model(r'Fire-64x64-color-v7-soft.h5')
 
-cap = cv2.VideoCapture('Datasets/Our_Complete_Dataset_Video_and_extra_NoFire_Frames/fire/FireVid20.mp4')
+cap = cv2.VideoCapture(r'VIDEO_FILE_NAME')
 time.sleep(2)
 
-if cap.isOpened():  # try to get the first frame
+if cap.isOpened(): # try to get the first frame
     rval, frame = cap.read()
 else:
     rval = False
@@ -39,8 +20,11 @@ else:
 IMG_SIZE = 64
 # IMG_SIZE = 224
 
+
 #for i in range(2500):
 #    cap.read()
+
+
 
 while(1):
 
@@ -62,19 +46,19 @@ while(1):
         print("Fire Probability: ", fire_prob)
         print("Predictions: ", model.predict(image))
         print(image.shape)
-
+        
         label = "Fire Probability: " + str(fire_prob)
-        cv2.putText(orig, label, (10, 25), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 255, 0), 2)
+        cv2.putText(orig, label, (10, 25),  cv2.FONT_HERSHEY_SIMPLEX,0.7, (0, 255, 0), 2)
 
-        cv2.namedWindow("Output")
         cv2.imshow("Output", orig)
-
+        
         key = cv2.waitKey(10)
-        if key == 27:  # exit on ESC
+        if key == 27: # exit on ESC
             break
-    elif rval == False:
-        break
+    elif rval==False:
+            break
 end = time.time()
+
 
 cap.release()
 cv2.destroyAllWindows()
