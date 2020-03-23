26dbbc291e2d0e4087806124013a0dce13e07ed0
expoli <me@expoli.tech>
2020-03-23 17:46:04 +0800

    reformat code. 解决使用 anaconda 安装的 opencv 无法使用的问题：
    error: (-2:Unspecified error) The function is not implemented. Rebuild the library with Windows, GTK+ 2.x or Carbon support. If you are on Ubuntu or Debian, install libgtk2.0-dev and pkg-config, then re-run cmake or configure script in function 'cvShowImage'
    
    解决方案：（Arch linux）
    
    install pkg-config (sudo pacman -Syu pkg-config),
    remove opencv from the environment (conda remove opencv) and
    re-install opencv from the conda-forge channel (conda install -c conda-forge opencv)

--- Codes/real_time_detection.py
+++ Codes/real_time_detection.py
@@ -1,17 +1,36 @@
+import time
+
+import cv2
+import numpy as np
 import tensorflow as tf
 from tensorflow.keras.models import load_model
 from tensorflow.keras.preprocessing.image import img_to_array
-import numpy as np
-import cv2
-import time
+
+
+def init_gpu():
+    gpus = tf.config.experimental.list_physical_devices('GPU')
+    if gpus:
+        try:
+            # Currently, memory growth needs to be the same across GPUs
+            for gpu in gpus:
+                tf.config.experimental.set_memory_growth(gpu, True)
+            logical_gpus = tf.config.experimental.list_logical_devices('GPU')
+            print(len(gpus), "Physical GPUs,", len(logical_gpus), "Logical GPUs")
+        except RuntimeError as e:
+            # Memory growth must be set before GPUs have been initialized
+            print(e)
+    return 0
+
+
+init_gpu()
 
 # loading the stored model from file
-model=load_model(r'Fire-64x64-color-v7-soft.h5')
+model = load_model('my_model.h5')
 
-cap = cv2.VideoCapture(r'VIDEO_FILE_NAME')
+cap = cv2.VideoCapture('Datasets/Our_Complete_Dataset_Video_and_extra_NoFire_Frames/fire/FireVid20.mp4')
 time.sleep(2)
 
-if cap.isOpened(): # try to get the first frame
+if cap.isOpened():  # try to get the first frame
     rval, frame = cap.read()
 else:
     rval = False
@@ -20,11 +39,8 @@ else:
 IMG_SIZE = 64
 # IMG_SIZE = 224
 
-
 #for i in range(2500):
 #    cap.read()
-
-
 
 while(1):
 
@@ -50,6 +66,7 @@ while(1):
         label = "Fire Probability: " + str(fire_prob)
         cv2.putText(orig, label, (10, 25),  cv2.FONT_HERSHEY_SIMPLEX,0.7, (0, 255, 0), 2)
 
+        cv2.namedWindow("Output")
         cv2.imshow("Output", orig)
         
         key = cv2.waitKey(10)
@@ -58,7 +75,6 @@ while(1):
     elif rval==False:
             break
 end = time.time()
-
 
 cap.release()
 cv2.destroyAllWindows()
